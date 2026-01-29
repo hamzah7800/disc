fb5eab4441f2f508c2361af6280ad66423a26946
hamzah7800 <hzeb7800pass@gmail.com>
2026-01-29 20:49:34 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'offline-chat-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
